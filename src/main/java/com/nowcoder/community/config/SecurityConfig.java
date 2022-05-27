4d495e368dee0866de1593171981c0fc393268f8
flipped0423 <60562497+flipped0423@users.noreply.github.com>
2022-05-27 10:08:19 +0800

    commit0527

---
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -0,0 +1,105 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.utils.CommunityConstant;
+import com.nowcoder.community.utils.CommunityUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author xindong
+ * @create 2022-05-26 20:32
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements CommunityConstant {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 授权
+        http.authorizeRequests()
+                .antMatchers(
+                        "/user/setting",
+                        "/user/upload",
+                        "/discuss/add",
+                        "/comment/add/**",
+                        "/letter/**",
+                        "/notice/**",
+                        "/like",
+                        "/follow",
+                        "/user/profile/**",
+                        "/unfollow"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_USER,
+                        AUTHORITY_ADMIN,
+                        AUTHORITY_MODERATOR
+                )
+                .antMatchers(
+                        "/discuss/top",
+                        "/discuss/wonderful"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_MODERATOR
+                )
+                .antMatchers(
+                        "/discuss/delete",
+                        "/data/**"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_ADMIN
+                )
+                .anyRequest().permitAll()
+                .and().csrf().disable();//禁用CSRF提交，防止CSRF攻击
+
+        // 权限不够时的处理
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    // 没有登录
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");//判断是否是异步请求
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {//是，返回json
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtils.getJSONString(403, "你还没有登录哦!"));
+                        } else {//否，重定向页面
+                            response.sendRedirect(request.getContextPath() + "/login");
+                        }
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    // 权限不足，和没有权限类似
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtils.getJSONString(403, "你没有访问此功能的权限!"));
+                        } else {
+                            response.sendRedirect(request.getContextPath() + "/denied");
+                        }
+                    }
+                });
+
+        // Security底层默认会拦截/logout请求,进行退出处理.
+        // 覆盖它默认的路径,才能执行我们自己的退出代码.
+        // 配置它logout的拦截路径到一个和本项目无关路径，使他不再拦截/logout
+        http.logout().logoutUrl("/securitylogout");
+    }
+}
--- src/main/java/com/nowcoder/community/interceptor/LoginTicketInterceptor.java
+++ src/main/java/com/nowcoder/community/interceptor/LoginTicketInterceptor.java
@@ -6,6 +6,10 @@ import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.utils.CookieUtil;
 import com.nowcoder.community.utils.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -41,6 +45,12 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
                 User user = userService.findUserById(loginTicket.getUserId());
                 //在本次请求中持有用户（同一线程）
                 hostHolder.setUser(user);
+
+                // 构建用户认证的结果,并存入SecurityContext,以便于Security进行授权.
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        user, user.getPassword(), userService.getAuthorities(user.getId()));
+                SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+
             }
         }
 
@@ -60,5 +70,7 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         hostHolder.clear();
+
+        //你在拦截器中清理掉登录信息，下次先访问过滤器是得不到登录信息的
     }
 }
